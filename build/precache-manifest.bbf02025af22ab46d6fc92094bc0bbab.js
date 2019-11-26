c710d465cbbbefda144cbb0c316413b8cea48969
Edgar Cheverier <edgar.aguilar@devex.com>
2019-11-26 11:55:52 +0100

    Part3 3.11 phonebook full stack

---
+++ build/precache-manifest.bbf02025af22ab46d6fc92094bc0bbab.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a72fcc82c1e023d20ede88ad8aee055",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b500ca8f25c9c0eaac72",
+    "url": "/static/js/2.e02e6f83.chunk.js"
+  },
+  {
+    "revision": "4f0a3ddfc124a36bea7b",
+    "url": "/static/js/main.44bd3deb.chunk.js"
+  },
+  {
+    "revision": "d436d1ba062a10e6162d",
+    "url": "/static/js/runtime-main.3b2c1c0f.js"
+  }
+]);
